d9cebe80ebc622aac2e82ea921b56d8a7054ba2f
Abdrakhman <rahman.abdirashov@gmail.com>
2026-06-21 12:09:08 +0500

    codecrafters submit [skip ci]

--- src/main.c
+++ src/main.c
@@ -4,8 +4,12 @@
 int main(int argc, char *argv[]) {
   // Flush after every printf
   setbuf(stdout, NULL);
+  char cmd[40];
 
   printf("$ ");
+
+  scanf("%s", cmd);
+  printf("%s: command not found\n", cmd);
 
   return 0;
 }
