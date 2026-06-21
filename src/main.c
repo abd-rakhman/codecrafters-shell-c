3866e4f0ff7307a4f5fca44d568dfe68f7a214e0
Abdrakhman <rahman.abdirashov@gmail.com>
2026-06-21 12:18:32 +0500

    codecrafters submit [skip ci]

--- src/main.c
+++ src/main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 int main(int argc, char *argv[]) {
   setbuf(stdout, NULL);
@@ -7,6 +8,9 @@ int main(int argc, char *argv[]) {
   printf("$ ");
 
   while (scanf("%s", cmd) == 1) {
+    if (strcmp(cmd, "exit") == 0) {
+      break;
+    }
     printf("%s: command not found\n", cmd);
     printf("$ ");
   }
