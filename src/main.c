5d0ac55a48ba094fbf5daf1451314e46149399d0
Abdrakhman <rahman.abdirashov@gmail.com>
2026-06-21 12:25:57 +0500

    codecrafters submit [skip ci]

--- src/main.c
+++ src/main.c
@@ -2,16 +2,27 @@
 #include <stdlib.h>
 #include <string.h>
 
+#define BUFFER_SIZE 1024
+
 int main(int argc, char *argv[]) {
   setbuf(stdout, NULL);
-  char cmd[40];
+  char cmd[BUFFER_SIZE];
   printf("$ ");
 
   while (scanf("%s", cmd) == 1) {
     if (strcmp(cmd, "exit") == 0) {
       break;
+    } else if (strcmp(cmd, "echo") == 0) {
+      char args[BUFFER_SIZE];
+      fgets(args, BUFFER_SIZE, stdin);
+      int start = 0;
+      while (args[start] == ' ') {
+        start++;
+      }
+      printf("%s", args + start);
+    } else {
+      printf("%s: command not found\n", cmd);
     }
-    printf("%s: command not found\n", cmd);
     printf("$ ");
   }
 
