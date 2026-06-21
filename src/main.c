6d74a9eb96978d4f4417fd848feb3dc4000341d4
Abdrakhman <rahman.abdirashov@gmail.com>
2026-06-21 12:16:53 +0500

    codecrafters submit [skip ci]

--- src/main.c
+++ src/main.c
@@ -2,14 +2,14 @@
 #include <stdlib.h>
 
 int main(int argc, char *argv[]) {
-  // Flush after every printf
   setbuf(stdout, NULL);
   char cmd[40];
-
   printf("$ ");
 
-  scanf("%s", cmd);
-  printf("%s: command not found\n", cmd);
+  while (scanf("%s", cmd) == 1) {
+    printf("%s: command not found\n", cmd);
+    printf("$ ");
+  }
 
   return 0;
 }
