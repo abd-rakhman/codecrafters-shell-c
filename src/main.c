a9c902e565d5d06d15fa16f970d50184ca08fc3e
Abdrakhman <rahman.abdirashov@gmail.com>
2026-06-28 12:13:25 +0500

    codecrafters submit [skip ci]

--- src/main.c
+++ src/main.c
@@ -121,7 +121,7 @@ int main(void) {
         if (!quote) quote = p;
         else if (*quote == *p) quote = NULL;
         else *(args[n] + len++) = *p;
-      } else if (*p == '\\') {
+      } else if (*p == '\\' && *quote != '\'') {
           p++;
           *(args[n] + len++) = *p;
       } else {
