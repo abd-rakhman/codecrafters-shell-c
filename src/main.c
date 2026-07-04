48bac76272b3de93873000820558c0332236a05d
Abdrakhman <rahman.abdirashov@gmail.com>
2026-07-04 08:10:01 +0500

    codecrafters submit [skip ci]

--- src/main.c
+++ src/main.c
@@ -145,7 +145,7 @@ int main(void) {
 
 
     if (n > 2) {
-      if ((strcmp(args[n-2], ">") == 0 || strcmp(args[n-2], "1>") == 0)) {
+      if (strcmp(args[n-2], ">") == 0 || strcmp(args[n-2], "1>") == 0) {
         int fd = open(args[n-1], O_WRONLY|O_CREAT|O_TRUNC, 0644);
         dup2(fd, 1);
         close(fd);
@@ -153,6 +153,18 @@ int main(void) {
         stdout_restore = true;
       } else if (strcmp(args[n-2], "2>") == 0) {
         int fd = open(args[n-1], O_WRONLY|O_CREAT|O_TRUNC, 0644);
+        dup2(fd, 2);
+        close(fd);
+        args[n-2] = NULL;
+        stderr_restore = true;
+      } else if (strcmp(args[n-2], ">>") == 0 || strcmp(args[n-2], "1>>") == 0) {
+        int fd = open(args[n-1], O_WRONLY|O_CREAT, 0644);
+        dup2(fd, 1);
+        close(fd);
+        args[n-2] = NULL;
+        stdout_restore = true;
+      } else if (strcmp(args[n-2], ">>") == 0 || strcmp(args[n-2], "1>>") == 0) {
+        int fd = open(args[n-1], O_WRONLY|O_CREAT, 0644);
         dup2(fd, 2);
         close(fd);
         args[n-2] = NULL;
